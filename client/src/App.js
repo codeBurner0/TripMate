a04758e8c1094ff8cff3a1bb047f854b972d3329
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2024-03-27 18:49:39 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState , useEffect} from 'react';
 import NavBar from './components/NavBar';
 import { BrowserRouter , Route , Routes, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
@@ -10,20 +11,33 @@ import AdminScreen from './screens/AdminScreen';
 import LandingScreen from './screens/LandingScreen';
 
 function App() {
+
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    // Read the data passed from the first app
+    const params = new URLSearchParams(window.location.search);
+    const dataFromFirstApp = params.get("data");
+    if ( localStorage.getItem("myData")==="null") {
+      // Set the data into localStorage of the second app
+      localStorage.setItem("myData", dataFromFirstApp);
+    }
+    setUser(JSON.parse(localStorage.getItem("myData")));
+  }, []);
+
   return (
     <div className="App">
       <NavBar/>
-      <BrowserRouter>
+
           <Routes>
               <Route  path ="/home" exact element={<HomeScreen/>} />
               <Route path="/book/:roomsid/:fromdate/:todate" element={<BookingScreen />}/>
-              <Route  path ="/register" exact element= {<RegisterScreen/>} />
-              <Route  path ="/login" exact element= {<LoginScreen/>} />
+              {/* <Route  path ="/register" exact element= {<RegisterScreen/>} />
+              <Route  path ="/login" exact element= {<LoginScreen/>} /> */}
               <Route  path ="/profile" element={<ProfileScreen />}/>
               <Route path="/admin" element={<AdminScreen/>}></Route>
               <Route path="/" element={<HomeScreen/>}></Route>
           </Routes>
-      </BrowserRouter>
+
     </div>
   );
 }
