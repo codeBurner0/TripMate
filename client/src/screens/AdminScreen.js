a760936e616c03770587b37cf3daf2271997df74
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2024-04-03 15:32:43 +0530

    Update AdminScreen.js

--- client/src/screens/AdminScreen.js
+++ client/src/screens/AdminScreen.js
@@ -218,7 +218,7 @@ export function Bookings(){
                  {users && (users.map( user=>{
                   return <tr>
                     <td>{user._id}</td>
-                    <td>{user.name}</td>
+                    <td>{user.firstName}</td>
                     <td>{user.email}</td>
                   </tr>
                  }))}
