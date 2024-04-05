107b613b13c4be8cfb3fccef86bf191b467719d4
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2024-04-05 13:13:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,72 @@
-import './App.css';
-import {useState , useEffect} from 'react';
-import NavBar from './components/NavBar';
-import { BrowserRouter , Route , Routes, Link, useNavigate } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import BookingScreen from './screens/BookingScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import LoginScreen from './screens/LoginScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import AdminScreen from './screens/AdminScreen';
-import LandingScreen from './screens/LandingScreen';
+import "./App.css";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import BookingScreen from "./screens/BookingScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import AdminScreen from "./screens/AdminScreen";
+import LandingScreen from "./screens/LandingScreen";
 
 function App() {
   const navigate=useNavigate()
+  const [data, setData] = useState({
+    _id: "660430fe74d07d6ebaba919f",
+    firstName: "Ankit",
+    lastName: "Anand",
+    email: "ankit@gmail.com",
+    phone: 7078259655,
+    __v: 0,
+  });
+  console.log(data.firstName)
 
-// localStorage.clear();
-// if(localStorage.getItem("myData")===null){
-//   localStorage.clear()
-// }
-// const params = new URLSearchParams(window.location.search);
-//     const dataFromFirstApp = params.get("data");
-//     console.log(!localStorage.getItem("myData"))
-//     if (!localStorage.getItem("myData")) {
-//       console.log("internal");
-//       localStorage.setItem("myData", dataFromFirstApp);
-//     }
-
-
-
-
-// localStorage.setItem("myData",)
   useEffect(() => {
-    if(localStorage.getItem("myData")==='null'){
-      localStorage.clear()
+    if (localStorage.getItem("myData") === "null") {
+      localStorage.clear();
     }
     const params = new URLSearchParams(window.location.search);
     const dataFromFirstApp = params.get("data");
-  //  if(JSON.parse(localStorage.getItem("myData"))?.email!==JSON.parse(dataFromFirstApp)?.email){
-  //     localStorage.clear()
-  //  }
-   console.log("hiol")
     if (!localStorage.getItem("myData")) {
       console.log("internal");
       localStorage.setItem("myData", dataFromFirstApp);
-      navigate('/home')
+      setData(JSON.parse(localStorage.getItem("myData")));
+      navigate("/home");
     }
   }, []);
 
-
-
-
-
-
-
-
-
-
-
-
   return (
     <div className="App">
-        <NavBar/>
+      <NavBar name={data.firstName} />
 
-<Routes>
-    <Route  path ="/home" exact element={<HomeScreen/>} />
-    <Route path="/book/:roomsid/:fromdate/:todate" element={<BookingScreen />}/>
-    {/* <Route  path ="/register" exact element= {<RegisterScreen/>} />
+      <Routes>
+        <Route path="/home" exact element={<HomeScreen />} />
+        <Route
+          path="/book/:roomsid/:fromdate/:todate"
+          element={<BookingScreen />}
+        />
+        {/* <Route  path ="/register" exact element= {<RegisterScreen/>} />
     <Route  path ="/login" exact element= {<LoginScreen/>} /> */}
-    <Route  path ="/profile" element={<ProfileScreen />}/>
-    <Route path="/admin" element={<AdminScreen/>}></Route>
-    <Route path="/" element={<HomeScreen/>}></Route>
-</Routes>
-
+        <Route
+          path="/profile"
+          element={
+            <ProfileScreen
+              name={data.firstName}
+              phone={data.phone}
+              email={data.email}
+            />
+          }
+        />
+        <Route path="/admin" element={<AdminScreen />}></Route>
+        <Route path="/" element={<HomeScreen />}></Route>
+      </Routes>
     </div>
   );
 }
