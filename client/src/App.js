64260480fa36d066b536012f5b056ddfe14ff7ea
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2024-04-05 12:25:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,40 @@ import NavBar from './components/NavBar';
 import { BrowserRouter , Route , Routes, Link, useNavigate } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import BookingScreen from './screens/BookingScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import AdminScreen from './screens/AdminScreen';
+import LandingScreen from './screens/LandingScreen';
 
 function App() {
   const navigate=useNavigate()
+
+// localStorage.clear();
+// if(localStorage.getItem("myData")===null){
+//   localStorage.clear()
+// }
+// const params = new URLSearchParams(window.location.search);
+//     const dataFromFirstApp = params.get("data");
+//     console.log(!localStorage.getItem("myData"))
+//     if (!localStorage.getItem("myData")) {
+//       console.log("internal");
+//       localStorage.setItem("myData", dataFromFirstApp);
+//     }
+
+
+
+
+// localStorage.setItem("myData",)
   useEffect(() => {
     if(localStorage.getItem("myData")==='null'){
       localStorage.clear()
     }
     const params = new URLSearchParams(window.location.search);
     const dataFromFirstApp = params.get("data");
-   // if(JSON.parse(localStorage.getItem("myData"))?.email!==JSON.parse(dataFromFirstApp)?.email){
-   //    localStorage.clear()
-   // }
+  //  if(JSON.parse(localStorage.getItem("myData"))?.email!==JSON.parse(dataFromFirstApp)?.email){
+  //     localStorage.clear()
+  //  }
    console.log("hiol")
     if (!localStorage.getItem("myData")) {
       console.log("internal");
@@ -25,6 +45,18 @@ function App() {
       navigate('/home')
     }
   }, []);
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
         <NavBar/>
@@ -32,6 +64,8 @@ function App() {
 <Routes>
     <Route  path ="/home" exact element={<HomeScreen/>} />
     <Route path="/book/:roomsid/:fromdate/:todate" element={<BookingScreen />}/>
+    {/* <Route  path ="/register" exact element= {<RegisterScreen/>} />
+    <Route  path ="/login" exact element= {<LoginScreen/>} /> */}
     <Route  path ="/profile" element={<ProfileScreen />}/>
     <Route path="/admin" element={<AdminScreen/>}></Route>
     <Route path="/" element={<HomeScreen/>}></Route>
